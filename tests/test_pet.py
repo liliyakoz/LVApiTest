7ae3b2991702b35ff3f6655a48af136bc0d86135
lkozyreva <liliya.kozyreva1997@gmail.com>
2025-07-24 12:52:35 +0300

    Добавила тест Добавление нового питомца

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,5 +1,7 @@
 import allure
 import requests
+import jsonschema
+from .schemas.pet_schema import PET_SCHEMA
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
@@ -28,6 +30,28 @@ class TestPet:
 
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
+
+    @allure.title("Добавление нового питомца")
+    def test_add_pet(self):
+        with allure.step("Подготовка данных для создания питомца"):
+            payload = {"id": 1,
+                       "name": "Bob",
+                       "status": "available"}
+
+        with allure.step("Отправка запроса на создание питомца"):
+            response = requests.post(url=f"{BASE_URL}/pet", json=payload)
+            response_json = response.json()
+        with allure.step("Проверка статуса ответа и валидация json"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+            jsonschema.validate(response.json(),PET_SCHEMA)
+
+        with allure.step("Проверка параметров в ответе"):
+            assert response_json['id'] == payload ['id'], "id питомца не совпадает с ожидаемым"
+            assert response_json ['name'] == payload['name'], "имя  питомца не совпадает с ожидаемым"
+            assert response_json ['status'] == payload['status'], "статус питомца не совпадает с ожидаемым"
+
+
+
 
 
 
